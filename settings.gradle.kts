ad9d4096b4098c2ad3ddafcdc85d5e4afcac46e0
MohammadNikdan <110032445+MohammadNikdan@users.noreply.github.com>
2025-09-03 18:34:22 +0330

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,15 @@
 pluginManagement {
     repositories {
-        // Remove original repositories and add Aliyun mirrors
-        maven("https://maven.aliyun.com/repository/google")
-        maven("https://maven.aliyun.com/repository/central")
+        google()
+        mavenCentral()
         gradlePluginPortal()
     }
 }
-
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        // Remove original repositories and add Aliyun mirrors
-        maven("https://maven.aliyun.com/repository/google")
-        maven("https://maven.aliyun.com/repository/central")
+        google()
+        mavenCentral()
     }
 }
 
