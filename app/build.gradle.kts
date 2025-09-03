385a13ce06cd8913f0eb60b7533ab9d558dc736a
MohammadNikdan <110032445+MohammadNikdan@users.noreply.github.com>
2025-09-03 18:47:47 +0330

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
 }
 
 android {
-    // این قسمت باید با نام پکیج شما یکی باشد
     namespace = "com.highflyers.grandmomplayer"
     compileSdk = 34
 
@@ -13,17 +12,11 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
         }
     }
     compileOptions {
@@ -33,10 +26,5 @@ android {
 }
 
 dependencies {
-    // این خط برای استفاده از کلاس‌هایی مثل AppCompatActivity ضروری است
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
